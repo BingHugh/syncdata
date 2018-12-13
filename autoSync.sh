85e712b5dd39ffdba89bfa1816244599ffabc02c
Bing Hu <hubing_hust@126.com>
2018-12-12 22:47:54 -0500

    call cleanupLocal.sh when a script is failed

--- autoSync.sh
+++ autoSync.sh
@@ -9,18 +9,46 @@ sh cleanupLocal.sh
 
 #export basic configurations from base database
 sh exportCfgData.sh
+if [ "$?" != 0 ]; then
+  echo "exportCfgData failed, exit now..."
+  sh cleanupLocal.sh
+  exit 1
+fi
+
 
 #export base data to local host
 sh exportBaseData.sh
+if [ "$?" != 0 ]; then
+  echo "exportBaseData failed, exit now..."
+  sh cleanupLocal.sh
+  exit 1
+fi
+
 
 #export trade data and then move them all to local host
 sh exportTradeData.sh
+if [ "$?" != 0 ]; then
+  echo "exportTradeData failed, exit now..."
+  sh cleanupLocal.sh
+  exit 1
+fi
+
 
 #dump table struct of base and trade database
 sh dumpTableStruct.sh
+if [ "$?" != 0 ]; then
+  echo "dumpTableStruct failed, exit now..."
+  sh cleanupLocal.sh
+  exit 1
+fi
 
 #import exported data to each database
 sh importData.sh
+if [ "$?" != 0 ]; then
+  echo "importData failed, exit now..."
+  sh cleanupLocal.sh
+  exit 1
+fi
 
 #clean up tmp files
 sh cleanupLocal.sh
--- cleanupLocal.sh
+++ cleanupLocal.sh
@@ -6,6 +6,7 @@ dir_tmp=/tmp/syncdata
 echo "clean up tmp files..."
 
 rm -rf /tmp/ta_t*
+rm -rf /tmp/exp_*
 rm -rf $dir_tmp
 
 echo "clean up tmp files done."
